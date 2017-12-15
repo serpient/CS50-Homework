edfc3715508472baddb98b742c915f5c684959c8
serpient <littlegangwolf@gmail.com>
2017-12-14 23:08:19 -0800

    update to pseudocode

--- PSET5/dictionary.c
+++ PSET5/dictionary.c
@@ -5,7 +5,6 @@
 #include "dictionary.h"
 
 // Returns true if word is in dictionary else false
-// check is case-insensitive. so convert word to lowercase before checking spelling
 bool check(const char *word)
 {
     // TODO
@@ -13,11 +12,31 @@ bool check(const char *word)
 }
 
 // Loads dictionary into memory, returning true if successful else false
-// loads dictionary into a data structure we have created
-// look into hashing
 bool load(const char *dictionary)
 {
-    // TODO
+    // open input file
+
+    // create initial array of length 26 + 1 (for null); will be an array of pointers, with its array index as its char identifier
+    // allocate memory for size of this array;
+    char* arrayOfPointers = malloc(27 * sizeof(void*));
+
+    // while not-end-of-file
+        // loop through dictionary word
+            // for each character, find the index within arrayOfPointers
+            // for arrayOfPointers[i character - 97 (ascii number for a)]
+                // if input character is null (end of dictionary word)
+                    // go through the linked list until it reaches the last pointer
+                    // point it to null within a copy of a 27 length array (so make it index 27)
+                    // return to beginning of function and read next word
+
+                // if at that index, there is a pointer that !== NULL
+                    // go to next dictionary word character
+                    // go to next pointer and see if it equals dictionary word character
+
+                // if pointer == null
+                    // make new iteration of arrayOfPointers and point the parent pointer to new array
+                    // then go and check on next dictionary word character
+
     return false;
 }
 
@@ -29,8 +48,6 @@ unsigned int size(void)
 }
 
 // Unloads dictionary from memory, returning true if successful else false
-// free any memory that was allocated in load. 
-// use valgrind to watch for leaks
 bool unload(void)
 {
     // TODO
